bcf106dc8bcb7ba51770c5e5d9b873f63c0c0785
Ramon de Paula Marques <ramonpm@gmail.com>
2017-07-10 13:35:02 -0300

    ticket list

--- src/app/ticket-form/ticket-form.component.ts
+++ src/app/ticket-form/ticket-form.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {Ticket} from '../models/ticket';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {ActiveStateService} from '../active-state.service';
+import {Angular2TokenService} from 'angular2-token';
 
 @Component({
   selector: 'app-ticket-form',
@@ -14,15 +15,39 @@ export class TicketFormComponent implements OnInit {
   ticket: Ticket = new Ticket();
   editTicket = false;
 
-  constructor(private state: ActiveStateService, private router: Router) {
+  constructor(private state: ActiveStateService,
+              public _tokenService: Angular2TokenService,
+              private route: ActivatedRoute,
+              private router: Router) {
   }
 
   ngOnInit() {
     this.editTicket = this.state.is('edit-ticket');
+    this.route.params.subscribe(params => {
+      const id = +params['id'];
+      if (id) {
+        this._tokenService.get('tickets/' + id).subscribe(
+          res => {
+            this.ticket = res.json();
+          },
+          error => console.log('Error retrieving ticket')
+        );
+      }
+    });
   }
 
   onSubmit() {
-
+    if (this.editTicket) {
+      this._tokenService.put('tickets/' + this.ticket.id, this.ticket).subscribe(
+        res => this.router.navigate(['/tickets']),
+        error => console.log('Error updating ticket')
+      );
+    } else {
+      this._tokenService.post('tickets', this.ticket).subscribe(
+        res => this.router.navigate(['/tickets']),
+        error => console.log('Error creating ticket')
+      );
+    }
   }
 
 }
--- src/app/tickets/tickets.component.ts
+++ src/app/tickets/tickets.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Ticket} from '../models/ticket';
+import {Angular2TokenService} from 'angular2-token';
 
 @Component({
   selector: 'app-tickets',
@@ -10,10 +11,25 @@ export class TicketsComponent implements OnInit {
 
   tickets: Array<Ticket>;
 
-  constructor() {
+  constructor(private _tokenService: Angular2TokenService) {
   }
 
   ngOnInit() {
+    this.getTickets();
+  }
+
+  getTickets() {
+    this._tokenService.get('tickets').subscribe(
+      res => this.tickets = res.json(),
+      error => console.log('Error retrieving tickets')
+    );
+  }
+
+  deleteTicket(ticket: Ticket) {
+    this._tokenService.delete('tickets/' + ticket.id).subscribe(
+      res => this.tickets.splice(this.tickets.indexOf(ticket), 1),
+      error => console.log('Error deleting ticket.')
+    );
   }
 
 }
